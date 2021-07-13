50a720b3bf322da8388a836ef75b6b0098e29e55
Sarah1908 <86227564+Sarah1908@users.noreply.github.com>
2021-07-13 22:26:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+function setup() {
+  createCanvas(400,400);
+
+
+}
+
+function draw() 
+{
+  background(30);
+
+
+  if (keyIsDown(DOWN_ARROW)){
+    background('lightPink')
+  }
+
+
+  if (keyIsDown(UP_ARROW)){
+    background('lightBlue')
+  }
+
+
+  if (keyIsDown(LEFT_ARROW)){
+    background('lightGreen')
+  }
+
+
+  if (keyIsDown(RIGHT_ARROW)){
+    background('yellow')
+  }
+}
+
+
+
+
